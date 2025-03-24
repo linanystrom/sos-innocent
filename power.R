65e33d27e41d4ebb965ce93bdc51e67adb5ff41e
Lina Nyström <lina.nystrom@psy.gu.se>
2025-03-24 14:37:45 +0100

    Randomization extended

--- power.R
+++ power.R
@@ -15,5 +15,5 @@ lapply(packages, library, character.only = TRUE)
 # ------------------------------------------------------------------------------
 
 pwr.t.test(n = 30, d = .86, sig.level = 0.05) # Effect size obtained from Oleszkiewicz & Watson (2020). 
-                                              # Difference between innocent and guilty suspects in early disclosure. 
+                                              # d = .86 = reflects difference between innocent and guilty suspects in early disclosure. 
 
--- randomization.R
+++ randomization.R
@@ -28,3 +28,23 @@ df$MC <- complete_ra(N = nrow(df),
                                          "MC5"))
 
 overall_count <- count(df, MC)
+
+write.csv(df,
+          "./random_assignment.csv",
+          row.names = FALSE)
+
+ra <- read.csv("random_assignment.csv")
+
+ra <- ra %>% 
+  mutate(
+    link = case_when(
+      MC == "MC3" ~ "https://samgu.eu.qualtrics.com/jfe/form/SV_byMwvFy8eNHzsJE",
+      MC == "MC4" ~ "https://samgu.eu.qualtrics.com/jfe/form/SV_eVPxLZ4liQjgIU6",
+      MC == "MC5" ~ "https://samgu.eu.qualtrics.com/jfe/form/SV_6PWdoXFv4Sr7Vyu"
+    )
+  )
+ra[,"code"] <- NA
+
+write.csv(ra,
+          "./random_assignment_ext.csv",
+          row.names = FALSE)
